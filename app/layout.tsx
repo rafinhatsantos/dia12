ba46d25ffde86370f661927129cce7719cd01789
Rafael <rafaeltsantos.dev@gmail.com>
2025-06-11 14:38:54 -0300

    testando

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,13 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
